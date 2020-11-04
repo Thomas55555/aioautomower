1495b922856e1193bd154a207f174da75e630238
Thomas55555 <59625598+Thomas55555@users.noreply.github.com>
2020-11-04 23:15:38 +0100

    bump to 0.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="husqvarna_automower",
-    version="0.4",
+    version="0.5",
     author="Thomas Protzner",
     author_email="thomas.protzner@gmail.com",
     description="module to communicate to Husqvarna Automower API",
