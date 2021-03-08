955c56dc44aa4a710914ee8d59691dcfd0db9398
Thomas5555 <thomas.protzner@gmail.com>
2021-03-09 00:13:46 +0100

    bump to version 2021.3.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="husqvarna_automower",
-    version="2021.3.1",
+    version="2021.3.2",
     author="Thomas Protzner",
     author_email="thomas.protzner@gmail.com",
     description="module to communicate to Husqvarna Automower API",
