c016b7e68d50d4a5f55e79059d967053efdc8624
Thomas5555 <thomas.protzner@gmail.com>
2021-03-14 15:22:04 +0100

    improve formatting

--- aioautomower/__init__.py
+++ aioautomower/__init__.py
@@ -8,12 +8,14 @@ import aiohttp
 _LOGGER = logging.getLogger(__name__)
 
 
-AUTH_API_URL = 'https://api.authentication.husqvarnagroup.dev/v1/oauth2/token'
-TOKEN_URL = 'https://api.authentication.husqvarnagroup.dev/v1/token'
-AUTH_HEADERS = {'Content-Type': 'application/x-www-form-urlencoded',
-                'Accept': 'application/json'}
+AUTH_API_URL = "https://api.authentication.husqvarnagroup.dev/v1/oauth2/token"
+TOKEN_URL = "https://api.authentication.husqvarnagroup.dev/v1/token"
+AUTH_HEADERS = {
+    "Content-Type": "application/x-www-form-urlencoded",
+    "Accept": "application/json",
+}
 
-MOWER_API_BASE_URL = 'https://api.amc.husqvarna.dev/v1/mowers/'
+MOWER_API_BASE_URL = "https://api.amc.husqvarna.dev/v1/mowers/"
 
 
 class GetAccessToken:
@@ -24,17 +26,23 @@ class GetAccessToken:
         self.username = username
         self.password = password
         self.api_key = api_key
-        self.auth_data = urlencode({'client_id': self.api_key, 'grant_type': 'password',
-                                    'username': self.username, 'password': self.password}, quote_via=quote_plus)
+        self.auth_data = urlencode(
+            {
+                "client_id": self.api_key,
+                "grant_type": "password",
+                "username": self.username,
+                "password": self.password,
+            },
+            quote_via=quote_plus,
+        )
 
     async def async_get_access_token(self):
         """Return the token."""
         async with aiohttp.ClientSession(headers=AUTH_HEADERS) as session:
             async with session.post(AUTH_API_URL, data=self.auth_data) as resp:
                 result = await resp.json(encoding="UTF-8")
-                result['status'] = resp.status
-        _LOGGER.debug(f"result: {result}")
-        _LOGGER.debug(f"resp.status: {resp.status}")
+                result["status"] = resp.status
+        _LOGGER.debug("Resp.status: %i", result["status"])
         return result
 
 
@@ -45,17 +53,22 @@ class RefreshAccessToken:
         """Initialize the Auth-API and store the auth so we can make requests."""
         self.api_key = api_key
         self.refresh_token = refresh_token
-        self.auth_data = urlencode({'client_id': self.api_key, 'grant_type': 'refresh_token',
-                                    'refresh_token': self.refresh_token}, quote_via=quote_plus)
+        self.auth_data = urlencode(
+            {
+                "client_id": self.api_key,
+                "grant_type": "refresh_token",
+                "refresh_token": self.refresh_token,
+            },
+            quote_via=quote_plus,
+        )
 
     async def async_refresh_access_token(self):
         """Return the refresh token."""
         async with aiohttp.ClientSession(headers=AUTH_HEADERS) as session:
             async with session.post(AUTH_API_URL, data=self.auth_data) as resp:
                 result = await resp.json(encoding="UTF-8")
-                result['status'] = resp.status
-        _LOGGER.debug(f"result: {result}")
-        _LOGGER.debug(f"resp.status: {resp.status}")
+                result["status"] = resp.status
+        _LOGGER.debug("Resp.status: %i", result["status"])
         return result
 
 
@@ -68,19 +81,21 @@ class GetMowerData:
         self.access_token = access_token
         self.provider = provider
         self.token_type = token_type
-        self.mower_headers = {'Authorization': '{0} {1}'.format(self.token_type, self.access_token),
-                              'Authorization-Provider': '{0}'.format(self.provider),
-                              'Content-Type': 'application/vnd.api+json',
-                              'X-Api-Key': '{0}'.format(self.api_key)}
+        self.mower_headers = {
+            "Authorization": "{0} {1}".format(self.token_type, self.access_token),
+            "Authorization-Provider": "{0}".format(self.provider),
+            "Content-Type": "application/vnd.api+json",
+            "X-Api-Key": "{0}".format(self.api_key),
+        }
 
     async def async_mower_state(self):
         """Return the mowers data as a list of mowers."""
         async with aiohttp.ClientSession(headers=self.mower_headers) as session:
             async with session.get(MOWER_API_BASE_URL) as resp:
                 result = await resp.json(encoding="UTF-8")
-                result['status'] = resp.status
-        _LOGGER.debug(f"result: {result}")
-        _LOGGER.debug(f"resp.status: {resp.status}")
+                result["status"] = resp.status
+        _LOGGER.debug("Result: %s", result)
+        _LOGGER.debug("Resp.status: %i", result["status"])
         return result
 
 
@@ -94,11 +109,13 @@ class Return:
         self.provider = provider
         self.token_type = token_type
         self.mower_id = mower_id
-        self.mower_headers = {'Authorization': '{0} {1}'.format(self.token_type, self.access_token),
-                              'Authorization-Provider': '{0}'.format(self.provider),
-                              'Content-Type': 'application/vnd.api+json',
-                              'accept': '*/*',
-                              'X-Api-Key': '{0}'.format(self.api_key)}
+        self.mower_headers = {
+            "Authorization": "{0} {1}".format(self.token_type, self.access_token),
+            "Authorization-Provider": "{0}".format(self.provider),
+            "Content-Type": "application/vnd.api+json",
+            "accept": "*/*",
+            "X-Api-Key": "{0}".format(self.api_key),
+        }
         self.mower_action_url = f"{MOWER_API_BASE_URL}{self.mower_id}/actions"
         self.payload = payload
 
@@ -107,9 +124,9 @@ class Return:
         async with aiohttp.ClientSession(headers=self.mower_headers) as session:
             async with session.post(self.mower_action_url, data=self.payload) as resp:
                 result = await session.close()
-                result['status'] = resp.status
-        _LOGGER.debug(f"sent payload {self.payload}")
-        _LOGGER.debug(f"API answer {resp.status}")
+                result["status"] = resp.status
+        _LOGGER.debug("Sent payload: %s", self.payload)
+        _LOGGER.debug("API answer: %i", result["status"])
         time.sleep(5)
         _LOGGER.debug("Waited 5s until mower state is updated")
         return resp.status
@@ -122,8 +139,10 @@ class DeleteAccessToken:
         """Initialize the Auth-API and store the auth so we can make requests."""
         self.api_key = api_key
         self.provider = provider
-        self.delete_headers = {'Authorization-Provider': '{0}'.format(self.provider),
-                               'X-Api-Key': '{0}'.format(self.api_key)}
+        self.delete_headers = {
+            "Authorization-Provider": "{0}".format(self.provider),
+            "X-Api-Key": "{0}".format(self.api_key),
+        }
         self.access_token = access_token
         self.delete_url = f"{TOKEN_URL}/{self.access_token}"
 
@@ -132,7 +151,7 @@ class DeleteAccessToken:
         async with aiohttp.ClientSession(headers=self.delete_headers) as session:
             async with session.delete(self.delete_url) as resp:
                 result = await resp.json(encoding="UTF-8")
-                result['status'] = resp.status
-        _LOGGER.debug(f"result: {result}")
-        _LOGGER.debug(f"resp.status: {resp.status}")
+                result["status"] = resp.status
+        _LOGGER.debug("Result: %s", result)
+        _LOGGER.debug("Resp.status: %i", result["status"])
         return result
