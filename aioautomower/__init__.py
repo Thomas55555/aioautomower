8c08cc7ca6f45c7a4f7e152a9855129b9b3f4f8d
Thomas5555 <thomas.protzner@gmail.com>
2021-04-05 21:08:23 +0200

    add validate access token

--- aioautomower/__init__.py
+++ aioautomower/__init__.py
@@ -80,6 +80,32 @@ class RefreshAccessToken:
         result["status"] = resp.status
         return result
 
+class ValidateAccessToken:
+    """Class to validate the Access Token."""
+
+    def __init__(self, api_key, access_token, provider):
+        """Initialize the Auth-API and store the auth so we can make requests."""
+        self.api_key = api_key
+        self.access_token = access_token
+        self.provider = provider
+        self.token_url = f"{TOKEN_URL}/{self.access_token}"
+        self.token_headers = {
+            "Authorization-Provider": "{0}".format(self.provider),
+            "Accept": "application/json",
+            "X-Api-Key": "{0}".format(self.api_key),
+        }
+
+    async def async_validate_access_token(self):
+        """Return the refresh token."""
+        async with aiohttp.ClientSession(headers=self.token_headers) as session:
+            async with session.get(self.token_url) as resp:
+                _LOGGER.debug("Resp.status validate token: %i", resp.status)
+                if resp.status == 200:
+                    result = await resp.json(encoding="UTF-8")
+                if resp.status >= 400:
+                    resp.raise_for_status()
+        result["status"] = resp.status
+        return result
 
 class GetMowerData:
     """Class to communicate with the Automower Connect API."""
