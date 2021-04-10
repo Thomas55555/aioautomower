61d1bf36b5cb1352139b9de5260b8a2bc2cfc749
Thomas5555 <thomas.protzner@gmail.com>
2021-04-10 17:38:43 +0200

    raise token error, when refreshing not possible

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="aioautomower",
-    version="2021.4.2",
+    version="2021.4.3",
     author="Thomas Protzner",
     author_email="thomas.protzner@gmail.com",
     description="module to communicate to Husqvarna Automower API",
