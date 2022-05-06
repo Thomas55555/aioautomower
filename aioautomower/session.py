3d1e6c50efa70179b29092e73292253f930ee7c4
Thomas5555 <thomas.protzner@gmail.com>
2022-05-06 14:04:44 +0000

    increase min_sleep_time

--- aioautomower/session.py
+++ aioautomower/session.py
@@ -216,7 +216,7 @@ class AutomowerSession:
             if self.token["status"] == 200 and "expires_at" in self.token:
                 expires_at = self.token["expires_at"]
 
-                sleep_time = max(0, expires_at - time.time() - MARGIN_TIME)
+                sleep_time = max(1, expires_at - time.time() - MARGIN_TIME)
             else:
                 sleep_time = MIN_SLEEP_TIME
 
