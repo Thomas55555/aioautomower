f6145a4a8e57fda52308ab4acb1104e9ff9d247f
Thomas5555 <thomas.protzner@gmail.com>
2022-06-13 14:28:11 +0000

    bump version to 2022.6.0

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     install_requires=list(val.strip() for val in open("requirements.txt")),
-    version="2022.5.3",
+    version="2022.6.0",
     entry_points={
         "console_scripts": ["automower=aioautomower.cli:main"],
     },
