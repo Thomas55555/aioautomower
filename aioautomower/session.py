46b056c35f65632230b44e71ec7053932a196bd0
Thomas5555 <thomas.protzner@gmail.com>
2022-08-12 17:02:26 +0000

    don't use copy

--- aioautomower/session.py
+++ aioautomower/session.py
@@ -232,7 +232,7 @@ class AutomowerSession:
                         tasks = j["attributes"]["calendar"]["tasks"]
                         if len(tasks) == 0:
                             temp_task = datum["attributes"]["calendar"]["tasks"]
-                            datum["attributes"][attrib] = j["attributes"][attrib].copy()
+                            datum["attributes"][attrib] = j["attributes"][attrib]
                             datum["attributes"]["calendar"]["tasks"] = temp_task
                         if len(tasks) > 0:
                             datum["attributes"][attrib] = j["attributes"][attrib]
