9ee35a1b84f4b5865b55db553e37b120ada4bdb2
Thomas55555 <59625598+Thomas55555@users.noreply.github.com>
2023-09-12 00:39:50 +0200

    Update const.py

--- aioautomower/const.py
+++ aioautomower/const.py
@@ -76,6 +76,31 @@ class MowerModes(StrEnum):
     UNKNOWN = "UNKNOWN"
 
 
+class RestrictedReasons(StrEnum):
+    """Restricted reasons in the planner of lawn mower."""
+
+    NONE = "NONE"
+    WEEK_SCHEDULE = "WEEK_SCHEDULE"
+    PARK_OVERRIDE = "PARK_OVERRIDE"
+    SENSOR = "SENSOR"
+    DAILY_LIMIT = "DAILY_LIMIT"
+    FOTA = "FOTA"
+    FROST = "FROST"
+    ALL_WORK_AREAS_COMPLETED = "ALL_WORK_AREAS_COMPLETED"
+    EXTERNAL = "EXTERNAL"
+
+
+class ExternalReasons(Enum):
+    GOOGLE_ASSISTANT = range(1000, 1999)
+    AMAZON_ALEXA = range(2000, 2999)
+    DEVELOPER_PORTAL = range(3000, 3999), range(200000, 299999)
+    IFTT = 4000, range(4003, 4999)
+    IFTT_WILDLIFE = 4001
+    IFTT_FROST_AND_RAIN = 4002
+    IFTT_CALENDAR_CONNECTION = 4003
+    IFTT_APPLETS = range(100000, 199999)
+
+
 ERRORCODES = {
     0: "Unexpected error",
     1: "Outside working area",
