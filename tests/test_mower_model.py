70413d6b7f46843c84bcb284f6805a36a30016ec
Thomas55555 <59625598+Thomas55555@users.noreply.github.com>
2024-02-14 19:09:27 +0100

    snapshot jwt

--- tests/test_mower_model.py
+++ tests/test_mower_model.py
@@ -5,7 +5,7 @@ from dataclasses import fields
 
 import pytest
 
-from aioautomower.utils import async_structure_token, mower_list_to_dictionary_dataclass
+from aioautomower.utils import mower_list_to_dictionary_dataclass
 from tests import load_fixture
 
 MOWER_ID = "c7233734-b219-4287-a173-08e3643f89f0"
@@ -56,7 +56,7 @@ async def test_high_feature_mower() -> None:
     assert len(mowers[MOWER_ID].positions) != 0
 
 
-def test_snapshot(snapshot):
+def test_mower_snapshot(snapshot):
     """Testing a snapshot of a high feature mower."""
     mower_fixture = load_fixture("high_feature_mower.json")
     mower_python = json.loads(mower_fixture)
@@ -65,14 +65,3 @@ def test_snapshot(snapshot):
         field_name = field.name
         field_value = getattr(mowers[MOWER_ID], field_name)
         assert field_value == snapshot(name=f"{field_name}")
-
-
-@pytest.mark.asyncio
-async def test_decode_token() -> None:
-    """Test converting a low feature mower."""
-    token_fixture = load_fixture("jwt")
-    token_structered = await async_structure_token(token_fixture)
-    assert token_structered.scope == "iam:read amc:api"
-    assert token_structered.client_id == "433e5fdf-5129-452c-xxxx-fadce3213042"
-    assert token_structered.user.first_name == "Erika"
-    assert token_structered.user.last_name == "Mustermann"
