c54881a2ec0f74cf70f0450a2848995524b4292b
Thomas55555 <59625598+Thomas55555@users.noreply.github.com>
2024-03-14 16:14:54 +0100

    make statistics optional

--- aioautomower/example.py
+++ aioautomower/example.py
@@ -16,8 +16,8 @@ from aioautomower.utils import async_get_access_token, structure_token
 _LOGGER = logging.getLogger(__name__)
 
 
-CLIENT_ID = "1e33fa27-ca34-4762-9a9e-5967f873a733"
-CLIENT_SECRET = "763adf3c-1b16-4c3b-91cd-c07316243880"
+CLIENT_ID = "7ac6b068-7f58-4749-aaa3-607167543933"
+CLIENT_SECRET = "2332940c-8191-4dcd-9d8e-5212cc3a3c8e"
 CLOCK_OUT_OF_SYNC_MAX_SEC = 20
 MAX_WS_RECONNECT_TIME = 600
 
