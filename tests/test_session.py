7d4c1da5de1e277870ac7539e07999397441964d
Thomas55555 <59625598+Thomas55555@users.noreply.github.com>
2024-04-15 19:52:21 +0200

    commands

--- tests/test_session.py
+++ tests/test_session.py
@@ -15,13 +15,15 @@ MOWER_ID = "c7233734-b219-4287-a173-08e3643f89f0"
 async def test_connect(snapshot: SnapshotAssertion):
     """Test automower session."""
     auth_mock = AsyncMock()
-    session = AutomowerSession(auth_mock)
-
-    # Call the connect method and assert
-    await session.connect()
+    automower_api = AutomowerSession(auth_mock)
+    await automower_api.connect()
     auth_mock.get_json.return_value = json.loads(load_fixture("raw_data.json"))
-    data = await session.get_status()
+    data = await automower_api.get_status()
     for field in fields(data[MOWER_ID]):
         field_name = field.name
         field_value = getattr(data[MOWER_ID], field_name)
         assert field_value == snapshot(name=f"{field_name}")
+    await automower_api.resume_schedule(MOWER_ID)
+    await automower_api.pause_mowing(MOWER_ID)
+    await automower_api.park_until_next_schedule(MOWER_ID)
+    await automower_api.park_until_further_notice(MOWER_ID)
