1206b58c1e6c1c01e325bbf768d19a25f921bdfb
Thomas55555 <59625598+Thomas55555@users.noreply.github.com>
2024-05-16 20:34:29 +0200

    tests

--- tests/test_two_mower.py
+++ tests/test_two_mower.py
@@ -16,13 +16,15 @@ async def test_two_mower(mock_automower_client_two_mowers: AbstractAuth) -> None
     """Test converting a high feature mower."""
     automower_api = AutomowerSession(mock_automower_client_two_mowers, poll=True)
     await automower_api.connect()
+    assert automower_api.data[MOWER1_ID].battery.battery_percent == 100
+    assert automower_api.data[MOWER2_ID].battery.battery_percent == 50
     # Test event of other mower doesn't overwrite the data
-    msg = WSMessage(WSMsgType.TEXT, load_fixture("status_event_other_mower.json"), None)
-    automower_api._handle_text_message(msg)  # pylint: disable=protected-access
+    msg2 = WSMessage(WSMsgType.TEXT, load_fixture("status_event_mower2.json"), None)
+    automower_api._handle_text_message(msg2)  # pylint: disable=protected-access
     assert automower_api.data[MOWER1_ID].battery.battery_percent == 100
     assert automower_api.data[MOWER2_ID].battery.battery_percent == 99
-    msg = WSMessage(WSMsgType.TEXT, load_fixture("status_event_battery_50.json"), None)
-    automower_api._handle_text_message(msg)  # pylint: disable=protected-access
+    msg1 = WSMessage(WSMsgType.TEXT, load_fixture("status_event_battery_50.json"), None)
+    automower_api._handle_text_message(msg1)  # pylint: disable=protected-access
     assert automower_api.data[MOWER1_ID].battery.battery_percent == 50
     assert automower_api.data[MOWER2_ID].battery.battery_percent == 99
 
