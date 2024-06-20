8416e3bbc1708875cbebbcbac54a683d60e71d4a
Thomas55555 <59625598+Thomas55555@users.noreply.github.com>
2024-06-20 22:04:27 +0200

    typo

--- src/aioautomower/helper.py
+++ src/aioautomower/helper.py
@@ -1,6 +1,6 @@
 """Helper for Husqvarna Automower."""
 
-# As I can't import Calendar and Event directly in HomeAsistant,
+# As I can't import Calendar and Event directly in HomeAssistant,
 # because it's making tests fail I import it here. Be aware of the LICENSE:
 # https://github.com/allenporter/ical/blob/64380cefb6942f75e76d725d1cf01ee39fe63b43/LICENSE
 # Source code can be found here: https://github.com/allenporter/ical/
