e4411f30755a0fa9a5b5312126988d0175a5ea77
Thomas55555 <59625598+Thomas55555@users.noreply.github.com>
2024-06-28 15:01:29 +0000

    fix

--- src/aioautomower/model.py
+++ src/aioautomower/model.py
@@ -7,6 +7,7 @@ from datetime import UTC, datetime, timedelta
 from enum import Enum, StrEnum
 from re import sub
 
+from ical.event import Event
 from mashumaro import DataClassDictMixin, field_options
 
 from .const import ERRORCODES
