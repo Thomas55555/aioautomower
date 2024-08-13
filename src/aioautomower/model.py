a1fa5578ff3529677f256174c0e6dd07c3a1d068
Thomas55555 <59625598+Thomas55555@users.noreply.github.com>
2024-08-13 18:57:44 +0200

    Add can_confirm_error capability (#417)

--- src/aioautomower/model.py
+++ src/aioautomower/model.py
@@ -132,10 +132,11 @@ class Battery(DataClassDictMixin):
 class Capabilities(DataClassDictMixin):
     """Information about what capabilities the Automower has."""
 
+    can_confirm_error: bool = field(metadata=field_options(alias="canConfirmError"))
     headlights: bool
-    work_areas: bool = field(metadata=field_options(alias="workAreas"))
     position: bool
     stay_out_zones: bool = field(metadata=field_options(alias="stayOutZones"))
+    work_areas: bool = field(metadata=field_options(alias="workAreas"))
 
 
 @dataclass
