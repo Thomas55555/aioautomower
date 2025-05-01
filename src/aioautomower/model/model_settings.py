a42d8bb0ca36ae06043d8c4d980ed20139d07e81
Thomas55555 <thomas.protzner@gmail.com>
2025-05-01 21:51:58 +0200

    more

--- src/aioautomower/model/model_settings.py
+++ src/aioautomower/model/model_settings.py
@@ -6,12 +6,24 @@ from enum import StrEnum
 from mashumaro import DataClassDictMixin, field_options
 
 
+class HeadlightModes(StrEnum):
+    """Headlight modes of a lawn mower."""
+
+    ALWAYS_ON = "always_on"
+    ALWAYS_OFF = "always_off"
+    EVENING_ONLY = "evening_only"
+    EVENING_AND_NIGHT = "evening_and_night"
+
+
 @dataclass
 class Headlight(DataClassDictMixin):
     """DataClass for Headlight values."""
 
-    mode: str | None = field(
-        metadata=field_options(deserialize=lambda x: x.lower()), default=None
+    mode: HeadlightModes | None = field(
+        metadata=field_options(
+            deserialize=lambda x: HeadlightModes(x.lower()) if x else None
+        ),
+        default=None,
     )
 
 
@@ -23,12 +35,3 @@ class Settings(DataClassDictMixin):
     cutting_height: int | None = field(
         metadata=field_options(alias="cuttingHeight"), default=None
     )
-
-
-class HeadlightModes(StrEnum):
-    """Headlight modes of a lawn mower."""
-
-    ALWAYS_ON = "always_on"
-    ALWAYS_OFF = "always_off"
-    EVENING_ONLY = "evening_only"
-    EVENING_AND_NIGHT = "evening_and_night"
