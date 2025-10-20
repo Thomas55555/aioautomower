3bbe75fcb3ebeb660dd37f1dd40e55dc831aa003
Thomas55555 <thomas.protzner@gmail.com>
2025-10-20 22:23:13 +0200

    typehint

--- src/aioautomower/model/model_battery.py
+++ src/aioautomower/model/model_battery.py
@@ -1,7 +1,6 @@
 """Models for Automower Connect API - Battery."""
 
 from dataclasses import dataclass, field
-from datetime import timedelta
 
 from mashumaro import DataClassDictMixin, field_options
 
@@ -11,7 +10,7 @@ class Battery(DataClassDictMixin):
     """Information about the battery in the Automower."""
 
     battery_percent: int = field(metadata=field_options(alias="batteryPercent"))
-    remaining_charging_time: timedelta | None = field(
+    remaining_charging_time: int | None = field(
         metadata=field_options(
             alias="remainingChargingTime",
             deserialize=lambda x: None if x == 0 else x,
